b44ebbf0a8ee78d218d584d1eb1c4144f9444bf4
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-04 13:56:44 +0300

    In progress

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+let start = 50
+let Destination = 42
+function distanceFromHqInBlocks (Start,Destination){
+    console.log(start-Destination)
+}
+
+distanceFromHqInBlocks(start,Destination)
