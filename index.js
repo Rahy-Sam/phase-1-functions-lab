17be3cf2be40882e4dfebad79d29b40be6dd1170
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-06 13:10:48 +0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let start = 50
-let Destination = 42
-function distanceFromHqInBlocks (Start,Destination){
-    console.log(start-Destination)
+let destination = 42
+function distanceFromHqInBlocks (start,destination){
+    console.log(destination-start)
 }
 
-distanceFromHqInBlocks(start,Destination)
+distanceFromHqInBlocks(start,destination)
+
+let feet = 264
+function distanceFromHqInFeet (){
+    console.log((destination-start)*feet)
+}
+distanceFromHqInFeet()
+
