dd32ced2b7fdf6c53a896662833da01f237b3f01
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-06 13:11:04 +0300

    Done

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ function distanceFromHqInFeet (){
 }
 distanceFromHqInFeet()
 
+
+ function calculateFarePrice(start,destination){
+    let feet =(distanceFromHqInFeet(start,destination))
+    if (feet<=400){
+        return 0;
+    }
+    else if (feet >400 && feet <= 2000){
+        return (feet-400)*0.02;
+    }
+    else if (feet > 2000 && feet<= 2500){
+        return 25
+    }
+    else if (feet > 2500){
+        return 'cannnot travel that far'
+    }
+    console.log(calculateFarePrice)
+ }
+ calculateFarePrice(start,destination)
